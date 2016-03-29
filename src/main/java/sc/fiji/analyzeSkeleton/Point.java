53ce6338b55f6e29365c080bd5aa1d2617f967d4
Curtis Rueden <ctrueden@wisc.edu>
2016-03-29 11:42:54 -0500

    Standardize copyright notices
    They are now generated and updated via Maven:
    
        mvn license:update-file-header license:update-project-license

--- src/main/java/sc/fiji/analyzeSkeleton/Point.java
+++ src/main/java/sc/fiji/analyzeSkeleton/Point.java
@@ -1,24 +1,26 @@
-package sc.fiji.analyzeSkeleton;
-
-
-/**
- * AnalyzeSkeleton_ plugin for ImageJ(C).
- * Copyright (C) 2008,2009 Ignacio Arganda-Carreras 
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation (http://www.gnu.org/licenses/gpl.txt )
- *
+/*
+ * #%L
+ * AnalyzeSkeleton_ plugin for ImageJ.
+ * %%
+ * Copyright (C) 2008 - 2016 Ignacio Arganda-Carreras.
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  * 
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
- * 
+ * You should have received a copy of the GNU General Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/gpl-3.0.html>.
+ * #L%
  */
+package sc.fiji.analyzeSkeleton;
+
 /**
  * This class represents a 3D point or position on a 3D image. Therefore, the 
  * coordinates are integer.
